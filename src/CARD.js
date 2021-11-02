ecf412e293f5e80705571e18295be43827500645
Shafayat Alam <shafayatalam2004@gmail.com>
2021-11-02 08:12:11 -0400

    Made media query changes for responsize design

--- src/CARD.js
+++ src/CARD.js
@@ -32,7 +32,9 @@ export default function CARD({forwardedRef, name, picture, pictureLength, descri
         <>
           <div ref = {forwardedRef} className="card">
             <div className ="picture-container">{picture}</div>
-            <div className="description-container">{description}</div>
+            <div className="description-container">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore facere omnis sunt dolor molestias ipsa. Aliquid, est blanditiis! Eveniet quibusdam corporis quo nesciunt unde sit veniam est impedit. Distinctio numquam quasi, vitae, aperiam minima sed aliquam atque ex adipisci eveniet animi asperiores vel sint quos error quod. Sed aspernatur, delectus optio velit totam temporibus ullam cupiditate, obcaecati error accusantium commodi recusandae omnis hic dicta. Fuga inventore corrupti, tempore velit voluptate tempora nulla adipisci earum architecto impedit harum praesentium qui aliquam porro, esse facilis vero. Excepturi sit quidem, voluptatum voluptates ad consequatur corrupti deserunt, esse, iusto unde odio. Quo, facere error.
+            </div>
           </div>
         </>
     )
