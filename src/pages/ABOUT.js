f9c9e37576a0d1be4294262ac23663802aa75007
Shafayat Alam <shafayatalam2004@gmail.com>
2021-11-04 08:43:51 -0400

    made responsive screen size

--- src/pages/ABOUT.js
+++ src/pages/ABOUT.js
@@ -5,13 +5,13 @@ export default function ABOUT() {
     return (
         <>
             <div className="container1">
-                <div className="title">Hi! I am Shafayat</div>
+                <div className="title"><h1>Hi! I am Shafayat</h1></div>
                 <div className="description">
-                    <span className = "indent">Weird enough...</span>I learn things for fun and I have a passion for science and math. 
+                    <p><span className = "indent">Weird enough...</span>I learn things for fun and I have a passion for science and math. 
                     If that isn't weird enough for you, I love engineering! Everyone should love engineering. You find a problem, which should be easy because there TONS in the world, and then create something to solve the problem. There is something about creating...I am not sure why but I love making things. 
                     It all started many many years in a galaxy far far away, mini me was designing-drawing I should say-batman gagdets. I was at a really old age of 8, I am not sure why none of the gadgets I designed never worked. It all seemed so simple. Nevertheless, I marched on. Now the things I design and create actually work...sometimes. 
                     Surf through the projects page of projects I did for fun myself by teaching myself the skills and then trying to make what I wanted to make. Also, checkout some cool STEM projects I did in school through the robotics club and BNL SPARK Program(a student research oppurtunity program by Brookhaven National Lab).
-                    Checkout the contact page and shoot me an email if you want to talk. 
+                    Checkout the contact page and shoot me an email if you want to talk.</p>
                 </div>
             </div>
         </>
